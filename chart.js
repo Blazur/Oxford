028521f8e56d55224d709536ba368f914eef350d
Mason <maseh87@gmail.com>
2014-08-23 17:14:44 -0700

    updated angular module

--- chart.js
+++ chart.js
@@ -1,8 +1,17 @@
-.directive('lineChart', function() {
+angular.module('oxford', [])
+
+.directive('chart', function() {
   var chartIdCounter = Math.floor((Math.random()*1000)+1);
+  console.log(chartIdCounter);
   return {
     restrict: 'E',
+    scope: {
+      data: '=',
+      options: '=',
+      axis: '='
+    },
     link: function(scope, element, attrs) {
+      console.log(element, ' element');
        //Assigning id to the element
       var chartId;
       if(element.attr('id')) {
@@ -16,8 +25,10 @@
 
       //Preparing chart data and options
       var genData = {
-        bindto: '#' + chartId,
-        data: scope.data
+        bindto: '#' + element.attr('id'),
+        data: scope.data,
+        axis: scope.axis,
+        options: scope.options
       };
       console.log(genData, ' genData');
       genData.data.type = attrs.chart? attrs.chart : scope.data.type? scope.data.type : 'line';
@@ -28,12 +39,10 @@
       }
       //On data change, reload chart
       onDataChanged = function(data, oldData) {
+        console.log(data, ' data');
         if(chart) {
           chart.load(data);
-          console.log(data.columns.length);
-          console.log(oldData.columns.length);
           if(data.columns.length < oldData.columns.length) {
-            console.log('data' + oldData.columns.length);
             chart.unload(['data' + oldData.columns.length]);
           }
         }
@@ -52,4 +61,4 @@
       var chart = c3.generate(genData);
     }
   };
-})
+});

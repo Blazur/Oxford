c87f2a8534b3a83a9aa1fb7a0db95e5eaff3921d
Mason <maseh87@gmail.com>
2014-08-24 10:42:04 -0700

    added comments

--- oxford.js
+++ oxford.js
@@ -1,6 +1,7 @@
 angular.module('oxford', [])
 
 .directive('chart', function() {
+  //random number to attach to the chart id
   var chartIdCounter = Math.floor((Math.random()*1000)+1);
 
   return {
@@ -12,7 +13,7 @@ angular.module('oxford', [])
     },
 
     link: function(scope, element, attrs) {
-       //Assigning id to the element
+      //assign an id to the chart if it doesn't have one
       var chartId;
       if(element.attr('id')) {
         chartId = element.attr('id');
@@ -23,22 +24,21 @@ angular.module('oxford', [])
         chartIdCounter += 1;
       }
 
-      //Preparing chart data and options
-      var genData = {
+      //generate c3 chart data
+      var chartData = {
         bindto: '#' + element.attr('id'),
         data: scope.data,
         axis: scope.axis,
         options: scope.options
       };
-      genData.data.type = attrs.chart? attrs.chart : scope.data.type? scope.data.type : 'line';
+      chartData.data.type = attrs.chart? attrs.chart : scope.data.type? scope.data.type : 'line';
       if(scope.options) {
         Object.keys(scope.options).forEach(function(key) {
-          genData[key] = scope.options[key];
+          chartData[key] = scope.options[key];
         });
       }
-      //On data change, reload chart
+      //Reload the chart if the data changes
       onDataChanged = function(data, oldData) {
-      console.log(data);
         if(chart) {
           chart.load(data);
           if(data.columns.length < oldData.columns.length) {
@@ -48,18 +48,11 @@ angular.module('oxford', [])
       };
       scope.$watch('data', onDataChanged, true);
 
-      //On chart type change, reload chart
-      onChartChanged = function(chart) {
-        if(chart) {
-          scope.data.type = chart;
-          chart.load(data);
-        }
-      };
       scope.$watch(function() {
         return attrs.chart;
       });
       //Generating the chart
-      var chart = c3.generate(genData);
+      var chart = c3.generate(chartData);
     }
   };
 });

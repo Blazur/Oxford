39f9d1218491f9ed1e2feeddc18a1e4eb474a720
Mason <maseh87@gmail.com>
2014-08-26 10:50:55 -0700

    added tests for ox-chart directive

--- test/directives/chartSpec.js
+++ test/directives/chartSpec.js
@@ -1,9 +1,39 @@
 describe('test', function() {
-  beforeEach(module('oxford.directives.chart'));
-  beforeEach(inject(function($injector) {
+  var $scope, element, isolate;
 
+  beforeEach(module('oxford.directives.chart'));
+
+  beforeEach(inject(function($rootScope, $compile) {
+    $scope = $rootScope.$new();
+    $scope.data = {
+      columns: [
+        ['Profile Completion', 100, 90, 75, 88, 12, 40],
+        ['Interests Declared', 75, 99, 65, 12, 24, 63]
+      ],
+      type: 'area'
+    };
+   $scope.axis = {
+      x: {
+        type: 'category',
+        categories: ['Josh', 'Mase', 'Xianhui', 'James', 'Joe', 'That One Guy']
+      }
+    };
+    element = "<ox-chart data='data' axis='axis'></ox-chart>";
+    element = $compile(element)($scope);
+    $scope.$digest();
+    isolate = element.isolateScope();
   }));
-  it('should do something', function() {
-    expect(true).to.be(true);
+
+  it('should have isolate scope', function() {
+    expect(isolate).to.be.an('object');
   });
+
+  it('should have a data property on the isolate scope', function() {
+    expect(isolate.data).to.be.an('object');
+  });
+
+  it('should have an axis property on the isolate scope', function() {
+    expect(isolate.axis).to.be.an('object');
+  });
+
 });

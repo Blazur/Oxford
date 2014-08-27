28af7bcf3dbd8d5c2966b26e5b8be93dd3fbf950
Mason <maseh87@gmail.com>
2014-08-26 19:30:33 -0700

    added comments to the specs

--- test/directives/chartSpec.js
+++ test/directives/chartSpec.js
@@ -1,10 +1,12 @@
 describe('test', function() {
+  //load variables in closure scope to be used throughout tests
   var $scope, element, isolate;
-
+  //load the chart module to gain access to the chart directive
   beforeEach(module('oxford.directives.chart'));
-
+  //inject the rootscope and compile function to test the chart directive
   beforeEach(inject(function($rootScope, $compile) {
     $scope = $rootScope.$new();
+    //data that is used to populate the chart directive
     $scope.data = {
       columns: [
         ['Profile Completion', 100, 90, 75, 88, 12, 40],
@@ -24,7 +26,9 @@ describe('test', function() {
 
 
     element = "<ox-chart id='chart' data='data' axis='axis' options='options'></ox-chart>";
+    //complile the element to gain access to the link function
     element = $compile(element)($scope);
+    //digest the scope to register the element
     $scope.$digest();
     isolate = element.isolateScope();
   }));
@@ -40,9 +44,11 @@ describe('test', function() {
   it('should have an axis property on the isolate scope', function() {
     expect(isolate.axis).to.be.an('object');
   });
+
   it('should have an options property on the isolate scope', function() {
     expect(isolate.options).to.be.an('object');
   });
+
   it('should update chart', function() {
     $scope.data.columns.pop();
     $scope.$digest();

f10835a2c56ad8de8533b6bc48393852a5b3c2e0
Mason <maseh87@gmail.com>
2014-09-08 17:59:15 -0700

    chart class

--- src/scripts/main/directives/chart.js
+++ src/scripts/main/directives/chart.js
@@ -22,7 +22,7 @@
         options: '=',
         axis: '='
       },
-      template: '<div style="height: 300px;"></div>',
+      template: '<div class="chart" style="height: 300px;"></div>',
       replace: true,
       link: function(scope, element, attrs) {
         //assign an id to the chart if it doesn't have one

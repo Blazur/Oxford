9e749a731f884b43739c8216f690d6a22342457d
Mason <maseh87@gmail.com>
2014-09-17 14:38:24 -0700

    updated css

--- src/scripts/main/directives/drag.js
+++ src/scripts/main/directives/drag.js
@@ -7,7 +7,8 @@
     return function(scope, element, attr) {
       if(attr.draggable !== 'false') {
         Draggable.create(element, {
-        bounds: {top: 10, left: 10, width: 1000, height: 800}
+        bounds: {top: 10, left: 10, width: 1000, height: 800},
+        throwProps: true
         });
       }
     };

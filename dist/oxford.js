5ec6b7d6e26402f81a9349d14e38f55f64f14772
Mason <maseh87@gmail.com>
2014-09-17 02:33:00 -0700

    removed bullets from li's

--- dist/oxford.js
+++ dist/oxford.js
@@ -15571,11 +15571,46 @@
   angular.module('oxford.directives', [
     'oxford.directives.chart',
     'oxford.directives.dashboard',
-    'oxford.directives.toolbar'
+    'oxford.directives.toolbar',
+    'oxford.directives.list'
   ]);
 
 }());
 
+;(function() {
+  'use strict';
+
+  angular.module('oxford.directives.list', [
+
+  ])
+  .directive('oxList', function() {
+    return {
+      transclude: true,
+      replace: true,
+      restrict: 'EA',
+      scope: true,
+      template: '<ul class="ox-list">' +
+        '<div ng-transclude></div>' +
+      '</ul>',
+      link: function($scope, $element, $attr, navController) {
+      }
+    };
+  })
+  .directive('oxItem', function() {
+    return {
+      transclude: true,
+      replace: true,
+      restrict: 'EA',
+      scope: true,
+      template: '<li class="ox-item">' +
+        '<div ng-transclude></div>' +
+      '</li>',
+      link: function($scope, $element, $attr, navController) {
+
+      }
+    };
+  });
+}());
 ;(function() {
   'use strict';
 

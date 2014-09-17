0ce5c378253d5186a92461d6c088a31ddcdc5de2
Mason <maseh87@gmail.com>
2014-09-17 02:06:07 -0700

    added new list directive

---
+++ src/scripts/main/directives/list.js
@@ -0,0 +1,20 @@
+;(function() {
+  'use strict';
+
+  angular.module('oxford.directives.list', [
+
+  ])
+  .directive('oxList', function() {
+    return {
+      // transclude: true,
+      replace: true,
+      restrict: 'EA',
+      scope: true,
+      template: '<ul class="ox-list">' +
+        // '<div ng-transclude></div>' +
+      '</ul>',
+      link: function($scope, $element, $attr, navController) {
+      }
+    };
+  });
+}());

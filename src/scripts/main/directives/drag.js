29711b53452354152b18276d476cdab1ad40b48e
Mason <maseh87@gmail.com>
2014-09-17 14:00:40 -0700

    added draggable directive

---
+++ src/scripts/main/directives/drag.js
@@ -0,0 +1,15 @@
+;(function(){
+  'use strict';
+
+  angular.module('oxford.directives.drag', [])
+
+  .directive('draggable', function() {
+    return function(scope, element, attr) {
+      if(attr.draggable !== 'false') {
+        Draggable.create(element, {
+        bounds: {top: 10, left: 10, width: 1000, height: 800}
+        });
+      }
+    };
+  });
+}());

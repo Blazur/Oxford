2711cf82ed899fe9b0f542f2348fdd398f77d282
Mason <maseh87@gmail.com>
2014-09-18 14:16:28 -0700

    performing hit tests

--- src/scripts/main/directives/drag.js
+++ src/scripts/main/directives/drag.js
@@ -4,13 +4,31 @@
   angular.module('oxford.directives.drag', [])
 
   .directive('draggable', function() {
+    var gridWidth = 200;
+    var gridHeight = 100;
+    var bounds = document.getElementsByClassName('dashboard-content');
+    console.log(bounds, ' bounds');
     return function(scope, element, attr) {
       if(attr.draggable !== 'false') {
         Draggable.create(element, {
-          bounds: document.getElementsByClassName('dashboard-content'),
+          bounds: bounds,
           type: 'x,y',
-          edgeResistance: 0.85,
-          throwProps: true
+          edgeResistance: 0.90,
+          throwProps: true,
+          // liveSnap: true,
+          snap: {
+            x: function(endValue) {
+              return Math.round(endValue / gridWidth) * gridWidth;
+            },
+            y: function(endValue) {
+              return Math.round(endValue / gridHeight) * gridHeight;
+            }
+           },
+           onDrag: function(e) {
+            if(this.hitTest('.chart')) {
+              console.log('hit!');
+            }
+           }
         });
       }
     };

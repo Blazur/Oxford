bd6f22992874e490cefbffcb3d7e0a3d4378d1cb
Mason <maseh87@gmail.com>
2014-09-17 23:51:34 -0700

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp'),
 var paths = {
   scripts: ['src/lib/d3/d3.js', 'src/lib/c3/c3.js', 'src/scripts/**/*.js'],
   stylus: ['src/styles/main.styl'],
+  watchStylus: ['src/styles/**/*.styl'],
   css: ['src/styles/**/*.css']
   // html: ['src/']
 };
@@ -11,7 +12,8 @@ var paths = {
 gulp.task('stylus', function() {
   return gulp.src(paths.stylus)
     .pipe($.stylus())
-    .pipe(gulp.dest('dist/'));
+    .pipe(gulp.dest('dist/'))
+    .pipe($.notify({message: 'Stylus Compiled'}));
 });
 
 gulp.task('css', ['stylus'], function() {
@@ -48,6 +50,10 @@ gulp.task('uglify', ['concat', 'concat:min'], function() {
     .pipe($.uglify())
     .pipe(gulp.dest('dist/'));
 });
+
+gulp.task('watch', function() {
+  gulp.watch(paths.watchStylus, ['stylus']);
+});
 //task to tell travis to run karma start and run in phantom.js
 gulp.task('test', $.shell.task([
   'karma start karma.conf.js --browsers Firefox --single-run'
@@ -55,4 +61,4 @@ gulp.task('test', $.shell.task([
 
 gulp.task('build', ['jshint', 'uglify', 'css']);
 
-gulp.task('default', ['build']);
+gulp.task('default', ['build', 'watch']);

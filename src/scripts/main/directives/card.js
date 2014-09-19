0e0165e157e226dda8ce1a59740f2fd9972aa49f
Mason <maseh87@gmail.com>
2014-09-18 17:00:38 -0700

    saving original position and snapping back

--- src/scripts/main/directives/card.js
+++ src/scripts/main/directives/card.js
@@ -8,7 +8,7 @@
       restrict: 'EAC',
       replace: true,
       transclude: true,
-      template: '<div class="card-material" draggable>' +
+      template: '<div class="card-material" draggable id="ox">' +
         '<div ng-transclude></div>' +
       '</div>',
       link: function(scope, element, attr) {

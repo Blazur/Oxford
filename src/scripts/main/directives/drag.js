9df3b0fc9af9a8fd69d168cc35732654b53e8f5d
Mason <maseh87@gmail.com>
2014-09-18 17:00:38 -0700

    saving original position and snapping back

--- src/scripts/main/directives/drag.js
+++ src/scripts/main/directives/drag.js
@@ -6,8 +6,8 @@
   .directive('draggable', function() {
     var gridWidth = 200;
     var gridHeight = 100;
+    var startX, startY;
     var bounds = document.getElementsByClassName('dashboard-content');
-    console.log(bounds, ' bounds');
     return function(scope, element, attr) {
       if(attr.draggable !== 'false') {
         Draggable.create(element, {
@@ -15,7 +15,10 @@
           type: 'x,y',
           edgeResistance: 0.90,
           throwProps: true,
-          // liveSnap: true,
+          onPress: function() {
+            startX = this.x;
+            startY = this.y;
+          },
           snap: {
             x: function(endValue) {
               return Math.round(endValue / gridWidth) * gridWidth;
@@ -23,12 +26,19 @@
             y: function(endValue) {
               return Math.round(endValue / gridHeight) * gridHeight;
             }
-           },
-           onDrag: function(e) {
-            if(this.hitTest('.chart')) {
-              console.log('hit!');
+          },
+          onDrag: function(e) {
+            if(this.hitTest('div.card-material')) {
+              // console.log(this);
             }
-           }
+          },
+          onDragEnd: function() {
+            if(this.hitTest('div.card-material')) {
+              console.log('Done Dragging');
+              // console.log(this);
+              TweenLite.to(element, 0.7, {x: startX, y: startY, ease: Power2.easeInOut});
+            }
+          }
         });
       }
     };

142ebd380cf834c29bd7c85ea2deb5baf1726dec
Mason <maseh87@gmail.com>
2014-09-19 16:50:08 -0700

    new tests

--- test/directives/listSpec.js
+++ test/directives/listSpec.js
@@ -1,18 +1,26 @@
 describe('list', function() {
-  var $scope, element;
+  var $scope, list, item;
 
   beforeEach(module('oxford.directives.list'));
 
   beforeEach(inject(function($rootScope, $compile) {
-    $rootScope.$new();
+    $scope = $rootScope.$new();
 
-    element = '<ox-list></ox-list>';
-    element = $compile(element)($scope);
+    list = '<ox-list></ox-list>';
+    list = $compile(list)($scope);
+    item = $compile('<ox-item></ox-item>')($scope);
 
-    $scope.digest();
   }));
+
   it('should have a list class', function() {
-    console.log(element.class);
-    // expect(element)
+    expect(list.attr('class')).to.contain('ox-list');
   });
+  it('should have a list directive', function() {
+    $scope.$digest();
+    expect(list).to.be.an('object');
+  });
+  it('should have an item directive', function() {
+    expect(item).to.be.an('object');
+  });
+
 });

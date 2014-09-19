8dd9e8582b709ac079fa439be56566740f77f5d7
Mason <maseh87@gmail.com>
2014-09-19 15:38:41 -0700

    changed chart directive

--- src/scripts/main/directives/chart.js
+++ src/scripts/main/directives/chart.js
@@ -3,7 +3,7 @@
 
   angular.module('oxford.directives.chart', [])
 
-  .directive('oxChart', ['$timeout', function($timeout) {
+  .directive('oxChart', [function() {
 
     //color patterns for chart coloring
     var patterns = {
@@ -20,11 +20,15 @@
       scope: {
         data: '=',
         options: '=',
-        axis: '='
+        axis: '=',
+        chart: '='
       },
       template: '<div draggable class="chart"></div>',
       replace: true,
       link: function(scope, element, attrs) {
+        var chartId;
+        var options = element.attr('options');
+
         //available option to show gridlines for chart
         if(attrs.grid === 'true') {
           scope.grid = {
@@ -38,10 +42,6 @@
             show: true
           };
         }
-        //option to zoom in on chart
-        if(attrs.zoom === 'true') {
-          scope.zoom = { zoom: { enabled: true } };
-        }
         //ability to change the color pattern
         if(attrs.pattern) {
           scope.color = {};
@@ -50,9 +50,6 @@
           scope.color = {};
           scope.color.pattern = patterns.dark ;
         }
-
-
-        var chartId;
 
         if(element.attr('id')) {
           chartId = element.attr('id');
@@ -63,47 +60,44 @@
           chartIdCounter += 1;
         }
 
-        //will be called on click
-        // scope.data.onclick = function(d, elem) {
-        //   console.log(elem.style.fill, ' elem');
-        //   elem.style.fill = '#ce93d8';
-        //   console.log(d, ' d');
-        // };
-
         //generate c3 chart data
         var chartData = {
           bindto: '#' + element.attr('id'),
-          data: scope.data,
+          data: scope[options],
           axis: scope.axis,
-          options: scope.options,
           grid: scope.grid,
           subchart: scope.subchart,
           zoom: scope.zoom,
           color: scope.color,
-          // x: scope.x,
+          x: scope.x,
           size: {
             height: 300,
             width: 950
           }
         };
-        //assign a type of line if undefined
-        chartData.data.type = attrs.chart? attrs.chart : scope.data.type? scope.data.type : 'line';
 
-        //if scope.options are given replace the data with the options data
-        if(scope.options) {
-          Object.keys(scope.options).forEach(function(key) {
-            chartData[key] = scope.options[key];
-          });
+
+        if(!chartData.data) {
+          console.log('You must have an options attribute on your chart directive!');
         }
+
         //Reload the chart if the data changes
-        // scope.$watch('data', function(data, prevData) {
-        //   if(chart) {
-        //     chart.load(data);
-        //     if(data.columns.length < prevData.columns.length) {
-        //       chart.unload(['data' + prevData.columns.length]);
-        //     }
-        //   }
-        // });
+        scope.$watch('options', function(data, prevData) {
+          if(chart) {
+            chart.load(data);
+            if(data.columns) {
+              if(data.columns.length < prevData.columns.length) {
+                chart.unload(['options' + prevData.columns.length]);
+              }
+            }
+            if(data.rows) {
+              if(data.rows.length < prevData.rows.length) {
+                chart.unload(['options' + prevData.rows.length]);
+              }
+            }
+          }
+        });
+
         //ran if there are changes to the chart
         var onChartChanged = function(chart) {
           if(chart) {
@@ -111,6 +105,7 @@
             chart.load(data);
           }
         };
+
         //watch the chart for any changes
         scope.$watch(function() {
           return attrs.chart;
@@ -118,110 +113,10 @@
 
         //Generating the chart
         var chart = c3.generate(chartData);
-
-        //mocking data incoming from a server to test the $watch function
-        setInterval(function() {
-          $timeout(function() {
-            chart.load({
-              rows: [
-                ['data1', 'data2', 'data3'],
-                [20,180,400],
-                [40,150,310],
-                [70,120,470],
-                [50,170,400],
-                [80,200,380]
-              ]
-            });
-          }, 1000);
-          $timeout(function () {
-            chart.load({
-              columns: [
-                ['data1', 130, 120, 150, 140, 160, 150],
-                ['data4', 30, 20, 50, 40, 60, 50],
-              ],
-              unload: ['data2', 'data3'],
-            });
-          }, 2000);
-
-          $timeout(function () {
-            chart.load({
-              rows: [
-                ['data2', 'data3'],
-                [120, 300],
-                [160, 240],
-                [200, 290],
-                [160, 230],
-                [130, 300],
-                [220, 320],
-              ],
-              unload: 'data4',
-            });
-          }, 3000);
-
-          $timeout(function () {
-            chart.load({
-              columns:[
-                ['data4', 30, 20, 50, 40, 60, 50,100,200]
-              ],
-              type: 'bar'
-            });
-          }, 4000);
-
-          $timeout(function () {
-            chart.unload({
-              ids: 'data4'
-            });
-          }, 5000);
-
-          $timeout(function () {
-            chart.load({
-              columns:[
-                ['data2', null, 30, 20, 50, 40, 60, 50]
-              ]
-            });
-          }, 6000);
-
-          $timeout(function () {
-            chart.unload();
-          }, 7000);
-
-          $timeout(function () {
-            chart.load({
-              rows: [
-                ['data4', 'data2', 'data3'],
-                [90, 120, 300],
-                [40, 160, 240],
-                [50, 200, 290],
-                [120, 160, 230],
-                [80, 130, 300],
-                [90, 220, 320],
-              ],
-              type: 'bar'
-            });
-          }, 8000);
-
-          $timeout(function () {
-            chart.load({
-              rows: [
-                ['data5', 'data6'],
-                [190, 420],
-                [140, 460],
-                [150, 500],
-                [220, 460],
-                [180, 430],
-                [190, 520],
-              ],
-              type: 'line'
-            });
-          }, 9000);
-
-          $timeout(function () {
-            chart.unload({
-              ids: ['data2', 'data3']
-            });
-          }, 10000);
-        }, 11000);
-        /////////////////
+        console.log(scope, ' scope');
+        if(scope.chart) {
+          scope.chart = chart;
+        }
       }
     };
   }]);
---
+++ test/directives/dragSpec.js
@@ -0,0 +1,18 @@
+describe('Drag Directive', function() {
+
+  var $scope, element;
+
+  beforeEach(module('oxford.directives.drag'));
+
+  beforeEach(inject(function($rootScope, $compile) {
+
+    $scope = $rootScope.$new();
+    element = "<div class='card-material' draggable></div>";
+    element = $compile(element)($scope);
+    $scope.$digest();
+  }));
+  it('should have a draggable directive', function() {
+    console.log(element);
+    expect(element).to.be.an('object');
+  });
+});
